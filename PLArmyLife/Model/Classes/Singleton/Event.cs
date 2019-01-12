8ae1b0722daf792bce053fe31aa8c3478122580d
Халилов Рустам - LN <konversys@gmail.com>
2019-01-12 10:01:17 +0700

    Заготовка Singleton

---
+++ PLArmyLife/Model/Classes/Singleton/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLArmyLife.Model.Classes.Singleton
+{
+    class Event
+    {
+        public Event(int year, string title, string message)
+        {
+            Year = year;
+            Title = title;
+            Message = message;
+        }
+
+        public int Year { get; private set; }
+        public string Title { get; private set; }
+        public string Message { get; private set; }
+    }
+}
---
+++ PLArmyLife/Model/Classes/Singleton/History.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLArmyLife.Model.Classes.Singleton
+{
+    class History
+    {
+        private static History history;
+        private static List<Event> events;
+
+        public History GetHistory()
+        {
+            if (history == null)
+            {
+                history = new History();
+            }
+
+            return history;
+        }
+
+        public void AddEvent(Event @event)
+        {
+            events.Add(@event);
+        }
+        public List<Event> GetEventByYear(int year)
+        {
+            return events.Where(x => x.Year == year).ToList();
+        }
+        public Event GetEventByID(int id)
+        {
+            return events[id];
+        }
+        public List<Event> GetEventByTitle(string title)
+        {
+            return events.Where(x => x.Title == title).ToList();
+        }
+    }
+}
